16f055878bc86da64e5201d8489389f5f091e8af
tjmehta <tejesh.mehta@gmail.com>
2014-03-22 14:52:31 -0700

    removed lodash updated errorHandler

--- test/fixtures/createAppWithMiddleware.js
+++ test/fixtures/createAppWithMiddleware.js
@@ -6,7 +6,7 @@ module.exports = function createAppWithMiddleware (middleware) {
   app.use(express.json());
   app.use(express.urlencoded());
   app.use(app.router);
-  app.use(mw.errorHandler({ showStack: true }));
+  app.use(mw.errorHandler({ showStack: true, log: false }));
   app.all('/body',
     // inspect,
     middleware,
