1456a10ab8b72399e0850e32e80658eaa0e5efd4
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-01-07 15:59:45 -0800

    unset keypath support

--- test/unset.js
+++ test/unset.js
@@ -3,6 +3,7 @@ var mw = require('../index');
 var fno = require('fn-object');
 var createCounter = require('callback-count');
 var request = require('./lib/superdupertest');
+var keypather = require('keypather')();
 var values = function (obj) {
   return Object.keys(obj).map(function (key) {
     return obj[key];
@@ -16,6 +17,7 @@ describe('unset', function () {
   describe('mw.body().unset(keys..)', unsetKeys('body'));
   describe('mw.query().unset(keys..)', unsetKeys('query'));
   describe('mw.params().unset(keys..)', unsetKeys('params'));
+  describe('mw.body().unset(keys..)', unsetKeypath('body'));
 });
 
 function unsetKey (dataType) {
@@ -51,7 +53,8 @@ function unsetKeys (dataType) {
     before(function () {
       this.keys = ['key1', 'key2'];
       this.unsetKeys = ['key3', 'key4'];
-      this.app = createAppWithMiddleware(mw[dataType]().unset(this.unsetKeys[0], this.unsetKeys[1]));
+      this.app = createAppWithMiddleware(
+        mw[dataType]().unset(this.unsetKeys[0], this.unsetKeys[1], this.unsetKeys[2]));
     });
     it('should unset keys', function (done) {
       var data = {};
@@ -70,6 +73,41 @@ function unsetKeys (dataType) {
         .send(body)
         .expect(200)
         .expect(function (res) {
+          res.body.should.eql(expected);
+        })
+        .end(done);
+    });
+  };
+}
+
+function unsetKeypath (dataType) {
+  return function () {
+    before(function () {
+      this.keys = ['key1', 'key2'];
+      this.unsetKeys = ['key3', 'key4', 'key.path'];
+      this.app = createAppWithMiddleware(
+        mw[dataType]().unset(this.unsetKeys[0], this.unsetKeys[1], this.unsetKeys[2]));
+    });
+    it('should unset keys', function (done) {
+      var data = {};
+      data[this.keys[0]] = 'value';
+      data[this.keys[1]] = 'value';
+      data[this.unsetKeys[0]] = 'value';
+      data[this.unsetKeys[1]] = 'value';
+      keypather.set(data, this.unsetKeys[2], 'value');
+      var expected = {};
+      expected[this.keys[0]] = 'value';
+      expected[this.keys[1]] = 'value';
+      var body = dataType === 'body' ? data : {};
+      var query = dataType === 'query' ? data : {};
+      var params = dataType === 'params' ? values(data) : [];
+      request(this.app)
+        .post('/'+dataType, params, query)
+        .send(body)
+        .expect(200)
+        .expect(function (res) {
+          Object.keys(res.body.key).length.should.equal(0);
+          delete res.body.key;
           res.body.should.eql(expected);
         })
         .end(done);
